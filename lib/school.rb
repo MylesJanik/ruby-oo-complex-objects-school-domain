5e65113e30741eba95716a18de6dc8e81b860dee
Myles <mylesjanik@gmail.com>
2020-11-11 17:20:48 -0800

    commit tree aafeee970ddc551456897e9494f0605bcbf7c709

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+        @initialized_grades = []
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student_name, grade)
+        if !@initialized_grades.include?(grade)
+            @roster[grade] = []
+            @initialized_grades << grade
+        end
+        @roster[grade] << student_name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_roster = @roster
+        temp_name = "zzzzzzz"
+        @roster.each do |a, b|
+            sorted_roster[a] = b.sort
+        end
+        sorted_roster
+    end
+
+end
